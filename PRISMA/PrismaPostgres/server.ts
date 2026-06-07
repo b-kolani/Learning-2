784898e7474bfd57e4022f380a67d7073623bc9e
Biman Kolani <bkolani@e1r1p5.1337.ma>
2026-06-07 19:41:26 +0100

    Updates

--- PRISMA/PrismaPostgres/server.ts
+++ PRISMA/PrismaPostgres/server.ts
@@ -2,16 +2,25 @@ import http from 'http';
 import { prisma } from "./lib/prisma"
 
 const server = http.createServer(async (req, res) => {
-    if (req.url === '/') {
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        });
+        return res.end();
+    }
+    if (req.url === '/' && req.method === 'GET') {
+
         res.writeHead(200, {
-            "access-control-allow-origin": '*',
-            "content-type": 'application/json'
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": 'application/json'
         });
         return res.end(JSON.stringify({ message: 'Hello!' }));
     } else if (req.url === '/posts') {
         res.writeHead(200, {
-            "access-control-allow-origin": '*',
-            "content-type": "application/json"
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": "application/json"
         });
         const allPosts = await prisma.post.findMany({
             include: {
@@ -19,6 +28,26 @@ const server = http.createServer(async (req, res) => {
             }
         });
         return res.end(JSON.stringify({ allPosts }));
+    } else if (req.url === '/post' && req.method === 'POST') {
+        let body = '';
+        req.on('data', (chunk) => {
+            // console.log(chunk);
+            body += chunk;
+        });
+        req.on('end', async () => {
+            const data = JSON.parse(body);
+            const user = await prisma.user.create({
+                data,
+                include: {
+                    posts: true
+                }
+            });
+            res.writeHead(200, {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify({ message: 'Post created!' }));
+        })
     }
 });
 
