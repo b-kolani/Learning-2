cd539448fc5f853d674e5958228d3779e767f724
Biman Kolani <bkolani@e1r2p6.1337.ma>
2026-06-08 14:02:19 +0100

    updates

--- COURS_PROGRAMMATION_RESEAU/PROGRAMMATION_TCP_LINUX/clientTCP.c
+++ COURS_PROGRAMMATION_RESEAU/PROGRAMMATION_TCP_LINUX/clientTCP.c
@@ -7,11 +7,17 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+#define LG_MESSAGE 256
+
 int main()
 {
     int descripteurSocket;
     struct sockaddr_in pointDeRencontreDistant;
     socklen_t longueurAdresse;
+    char messageEnvoi[LG_MESSAGE];
+    char messageRecu[LG_MESSAGE];
+    int ecris, lus;
+    // int retour;
 
     //--- Début de l'étape numéro 1:
     // Créer une socket de communication
@@ -36,10 +42,11 @@ int main()
     // Renseigne la structure sockaddr_in avec les informations du serveur distant
     pointDeRencontreDistant.sin_family = PF_INET;
     // On choisit le numéro de port d'écoute du serveur
-    pointDeRencontreDistant.sin_port = htons(IPPORT_USERRESERVED);  // = 5000
+    pointDeRencontreDistant.sin_port = htons(IPPORT_USERRESERVED); // = 5000
     // On choisit l'adresse IPv4 du serveur
     // à modifier selon les besoins
-    if (inet_aton("127.0.0.1", &pointDeRencontreDistant.sin_addr) == 0) {
+    if (inet_aton("127.0.0.1", &pointDeRencontreDistant.sin_addr) == 0)
+    {
         perror("inet_aton");
         close(descripteurSocket);
         exit(-1);
@@ -49,12 +56,49 @@ int main()
     if ((connect(descripteurSocket,
                  (const struct sockaddr *)&pointDeRencontreDistant, longueurAdresse)) == -1)
     {
-        perror("connect"); // Affiche le message d'erreur
+        perror("connect");        // Affiche le message d'erreur
         close(descripteurSocket); // On ferme la ressource avant de quitter
-        exit(-2); // On sort en indiquant un code erreur
+        exit(-2);                 // On sort en indiquant un code erreur
     }
     //--- Fin de l'étape numéro 2 !
     printf("Connexion au serveur réussie avec succès ! \n");
+
+    //--- Début de l'étape numéro 4:
+    // initialisé à 0 les messages
+    memset(messageEnvoi, 0x0, LG_MESSAGE * sizeof(char));
+    memset(messageRecu, 0x0, LG_MESSAGE * sizeof(char));
+
+    sprintf(messageEnvoi, "Hello World!\n");
+    ecris = write(descripteurSocket, messageEnvoi, strlen(messageEnvoi)); // message à TAILLE variable
+    switch (ecris)
+    {
+        case -1: /* une erreur */
+            perror("write");
+            close(descripteurSocket);
+            exit(-3);
+        case 0: /* la socket a été fermée */
+            printf("La socket a été fermée par le serveur !\n\n");
+            close(descripteurSocket);
+            return 0;
+        default: /* envoie de n octets */
+            printf("Message %s envoyé avec succès (%d octets)\n\n", messageEnvoi, ecris);
+    }
+
+    /* Reception des données du serveur */
+    lus = read(descripteurSocket, messageRecu, LG_MESSAGE*sizeof(char)); /* attend un message 
+    de taille fixe  */
+    switch (lus) {
+        case -1: /* uen erreur */
+            perror("read");
+            close(descripteurSocket);
+            exit(-4);
+        case 0: /* la socket est fermée */
+            printf("La socket a été fermée par le serveur !\n\n");
+            return 0;
+        default: /* réception de n octets */
+            printf("Message reçu du serveur : %s (%d octets)\n\n", messageRecu, lus);
+    }
+    //--- Fin de l'étape numéro 4
 
     // On ferme la ressource avant de quitter
     close(descripteurSocket);
