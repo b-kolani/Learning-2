7fd1a2aeef3e4b871a40d2e8c2df43ab6f2cd863
Biman Kolani <bkolani@e1r2p6.1337.ma>
2026-06-09 13:58:02 +0100

    Updates

---
+++ JAVA/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        char myChar = 'D';
+        System.out.println(myChar);
+        char myGrade = 'B';
+        System.out.println(myGrade);
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println("ASCII chars: " + myVar1 + ", " + myVar2 + ", " + myVar3);
+        String myString = "Hello world!";
+        System.out.println("This expression will result to concatenation: " + 5 + 6);
+        System.out.println("This expression will be evaluated as an addition: " + (5 + 6));
+        System.out.println(myString);
+        int myIntNum = 100000;
+        System.out.println(myIntNum);
+        float myFloatNum = 5.75f;
+        System.out.println(myFloatNum);
+        double myDoubleNum = 19.99d;
+        System.out.println(myDoubleNum);
+        byte myByteNum = 100;
+        System.out.println(myByteNum);
+        short myShortNum = 5000;
+        System.out.println(myShortNum);
+        long    myLongNum = 15000000000L;
+        System.out.println(myLongNum);
+        float f1 = 35e3f;
+        System.out.println(f1);
+        double d1 = 12E4d;
+        System.out.println(d1);
+        String txt = "It's alright.";
+        System.out.println(txt);
+    }
+}
